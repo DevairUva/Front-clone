fcb198354efc16eb0285bbb312174ef10ef0efcd
DEVair Martins <devair.martins@gmail.com>
2023-03-26 16:55:06 -0300

    enviando email

--- Front-End-M5/src/components/CardComp.jsx
+++ Front-End-M5/src/components/CardComp.jsx
@@ -2,34 +2,42 @@ import Card from 'react-bootstrap/Card';
 import axios from "axios";
 import { useEffect, useState } from 'react';
 function CardComp() {
-    const [titulo,setTitulo]=useState('')
-    const [descricao,setDescricao]=useState('')
-    const [data_criacao,setData_criacao]=useState('')
+
+    const [nome, setNome] = useState('')
+    // const [descricao, setDescricao] = useState('')
+    // const [data_criacao, setData_criacao] = useState('')
+
     useEffect(() => {
         const options = { method: 'GET', url: 'http://localhost:3000/tarefa' };
         axios.request(options).then(function (response) {
-            setTitulo(response.data[0].titulo)
-            setDescricao(response.data[0].descricao)
-            setData_criacao(response.data[0].data_criacao)
+            setNome(response.data[0].nome)
+            // setDescricao(response.data[0].descricao)
+            // setData_criacao(response.data[0].data_criacao)
         }).catch(function (error) {
             console.error(error);
         });
-    },[])
+    }, [])
+
+    console.log(nome)
 
     return (
-        <Card>
-            <Card.Header>{titulo}</Card.Header>
-            <Card.Body>
-                <blockquote className="blockquote mb-0">
-                    <p>
-                        {descricao}
-                    </p>
-                    <footer className="blockquote-footer">
-                        {data_criacao}
-                    </footer>
-                </blockquote>
-            </Card.Body>
-        </Card>
+        <div>
+            <br/>
+            <Card>
+                <Card.Header>a</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            av
+                        </p>
+                        <footer className="blockquote-footer">
+                            b
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+            console.log(nome)
+        </div>
     );
 }
 
